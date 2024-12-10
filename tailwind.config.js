ac5234702bc0ca52aed63e10766578e404209be8
Mehedi Hasan <mehediextrim@gmail.com>
2024-12-10 20:52:57 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:[
+    // './components/**/*.{html,js}',
+    './nav-bar/**/*.html',
+    './work-process/**/*.html',
+    './banner/**/*.html',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
