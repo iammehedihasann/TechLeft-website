8d8b09b6de9875f7d398ff9a871b00396ec15e1c
Mehedi Hasan <mehediextrim@gmail.com>
2024-12-13 18:26:08 +0600

    Adding the images of work process section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:[
-    // './components/**/*.{html,js}',
+  content: [
+    // './components/**/*.{html,js}', 
     './nav-bar/**/*.html',
     './work-process/**/*.html',
     './banner/**/*.html',
+    './blogs/**/blog.html',
     './index.html',
   ],
   theme: {
@@ -12,4 +13,3 @@ module.exports = {
   },
   plugins: [],
 }
-
